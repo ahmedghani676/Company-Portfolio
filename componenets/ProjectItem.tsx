86e1072737da1b8e731137fa0de3f71498fa1cf6
ahmedghani676 <58558139+ahmedghani676@users.noreply.github.com>
2024-10-08 23:48:27 +0500

    ad

--- componenets/ProjectItem.tsx
+++ componenets/ProjectItem.tsx
@@ -28,12 +28,12 @@ const ProjectItem : React.FC<ProjectList> = ({title, backgroundImg, tech,descrip
     githubUrl
   };
     return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
-    <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' width={600} height={200}/> 
+<div className='relative flex items-center justify-center h-80 w-100 shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]'>
+    <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' width={400} height={150} /> 
     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-        <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
-        <p className='pb-4 pt-2 text-white text-center'>{tech}</p>
-        <p className='relative text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer' onClick={handleOpenPopup}>More Info</p>
+        <h3 className='text-lg text-white tracking-wider text-center'>{title}</h3>
+        <p className='pb-2 pt-1 text-white text-center text-sm'>{tech}</p>
+        <p className='relative text-center py-2 rounded-lg bg-white text-gray-700 font-bold text-sm cursor-pointer' onClick={handleOpenPopup}>More Info</p>
     </div>
  <ProjectPopUp isOpen={isPopupOpen} onClose={handleClosePopup} project={project}
  />
